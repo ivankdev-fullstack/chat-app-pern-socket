aa9e01a108ea535f3ce570ce7ba0331df5f61bcb
Ivan Kononenko <ivan.kononenko.dev@gmail.com>
2025-02-19 16:05:30 +0200

    backend feat: add auth and message routes & controllers; install and configure prisma schemas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,13 @@
+import * as dotenv from "dotenv";
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+dotenv.config();
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", messageRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running on port");
