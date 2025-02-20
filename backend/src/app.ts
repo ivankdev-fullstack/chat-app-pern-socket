1ee19c2db5155c62c707610a1eff6a08ae345f57
Ivan Kononenko <ivan.kononenko.dev@gmail.com>
2025-02-20 22:50:49 +0200

    backend feat: add cookie parser and middlewares for production mode

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,28 @@
+import cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
+const PORT = process.env.PORT || 3333;
 const app = express();
+const __dirname = path.resolve();
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/auth", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(3333, () => {
-  console.log("Server is running on port");
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
-export const signup = async (req: Request, res: Response) => {};
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+  } catch (err) {}
+};
+
 export const login = async (req: Request, res: Response) => {};
 export const logout = async (req: Request, res: Response) => {};
