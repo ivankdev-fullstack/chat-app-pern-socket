323b6950a3ab1dda0a270cb3a48dc872f6541f71
Ivan Kononenko <ivan.kononenko.dev@gmail.com>
2025-02-21 00:39:28 +0200

    frontend feat: add user auth context provider to check user auth status and redirect in case of it

--- frontend/src/components/messages/MessageContainer.tsx
+++ frontend/src/components/messages/MessageContainer.tsx
@@ -4,19 +4,17 @@ import Messages from "./Messages";
 // import { MessageCircle } from "lucide-react";
 
 const MessageContainer = () => {
-	return (
-		<div className='w-full flex flex-col'>
-			<>
-				{/* Header */}
-				<div className='bg-slate-500 px-4 py-2 mb-2'>
-					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-				</div>
-
-				<Messages />
-				<MessageInput />
-			</>
-		</div>
-	);
+  return (
+    <div className="w-full flex flex-col">
+      <div>
+        <div className="flex justify-center bg-slate-500 px-4 py-2 mb-2">
+          <span className="text-gray-900 font-bold">John doe</span>
+        </div>
+        <Messages />
+        <MessageInput />
+      </div>
+    </div>
+  );
 };
 export default MessageContainer;
 
---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,62 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type AuthUserType = {
+  id: string;
+  fullname: string;
+  email: string;
+  avatarImg: string;
+  gender: string;
+} | null;
+
+interface AuthContextType {
+  authUser: AuthUserType;
+  setAuthUser: Dispatch<SetStateAction<AuthUserType | null>>;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  authUser: null,
+  setAuthUser: () => {},
+  isLoading: true,
+});
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [authUser, setAuthUser] = useState<AuthUserType>(null);
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchAuthUser = async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+
+        setAuthUser(data);
+      } catch (err: any) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAuthUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
